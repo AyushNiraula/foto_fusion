df44fb46577121448b39d509169e9088da70db68
Ayush Niraula <ayush.niraula@office.mypay.com.np>
2024-11-25 16:52:01 +0545

    Setup : React, Tailwind, Vite, React Router DOM

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './globals.css';
+
+import { Route, Routes } from 'react-router-dom';
+const App = () => {
+  return (
+  <main className='flex h-screen'>
+<Routes>
+  {/* Public Routes */}
+
+  {/* Private Routes */}
+</Routes>
+  </main>
+  )
+}
+
+export default App
